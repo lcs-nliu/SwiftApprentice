62bc1c14de288bdbdfae8a06810257df718eb99a
Nicole Liu <nicoleliu_@outlook.com>
2019-11-22 14:33:49 -0500

    Initial Commit

---
+++ SwiftApprentice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftApprentice
+//
+//  Created by Liu, Bojun on 2019-11-22.
+//  Copyright © 2019 Liu, Nicole. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
